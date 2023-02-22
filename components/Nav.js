7f37e8539d871cb115af914f35864f9f7f8608dd
vincenzochiovaro <vincenzochiovaro194@gmail.com>
2023-02-22 12:03:41 +0000

    complete navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,9 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./Home";
 
-const addBtn = () => {};
-const offers = () => {};
-const user = () => {};
+const AddBtn = () => {};
+const Offers = () => {};
+const User = () => {};
 
 const Tab = createBottomTabNavigator();
 
@@ -11,9 +11,9 @@ const Nav = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="add" component={addBtn} />
-      <Tab.Screen name="offers" component={offers} />
-      <Tab.Screen name="user" component={user} />
+      <Tab.Screen name="+" component={AddBtn} />
+      <Tab.Screen name="Offers" component={Offers} />
+      <Tab.Screen name="User" component={User} />
     </Tab.Navigator>
   );
 };
