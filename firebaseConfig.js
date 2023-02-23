679cf82df0b46e68e5ae37b48cec297576d5d3b5
kelvinkml <kelvinkml@live.com>
2023-02-23 16:38:16 +0000

    adds getting all items without infinite loop

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 // import firebase from 'firebase'
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from "firebase/firestore"
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,15 +10,15 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAmk3z2YCy3aQ3dLOMhOns42ZY5EdE0Y8A",
-    authDomain: "swapz-app.firebaseapp.com",
-    projectId: "swapz-app",
-    storageBucket: "swapz-app.appspot.com",
-    messagingSenderId: "19710547302",
-    appId: "1:19710547302:web:29dc90fb691d09e77024a7"
-  };
+  apiKey: "AIzaSyBB7MLSPg_8ERZO5yNbw8fnE42rxcJaWD8",
+  authDomain: "swapz-f37c8.firebaseapp.com",
+  projectId: "swapz-f37c8",
+  storageBucket: "swapz-f37c8.appspot.com",
+  messagingSenderId: "312862480096",
+  appId: "1:312862480096:web:70f8eeb0f4154eda196285",
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
-export const auth = getAuth(app)
+export const db = getFirestore(app);
+export const auth = getAuth(app);
