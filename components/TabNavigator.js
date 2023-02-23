cc5f4e45cf9c6a3b83a272a22b928517417f35fa
vincenzochiovaro <vincenzochiovaro194@gmail.com>
2023-02-23 11:46:26 +0000

    add navbar icons

--- components/TabNavigator.js
+++ components/TabNavigator.js
@@ -1,16 +1,50 @@
 import Home from "./Home";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AddItem from "./AddItem";
+import { AntDesign } from "@expo/vector-icons";
 const User = () => {};
 const Offers = () => {};
 const Tab = createBottomTabNavigator();
+
 function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Add Item" component={AddItem} />
-      <Tab.Screen name="User" component={User} />
-      <Tab.Screen name="Offers" component={Offers} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesign name="home" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Add Item"
+        component={AddItem}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesign name="pluscircle" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={User}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesign name="user" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Swaps"
+        component={Offers}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesign name="swap" color={color} size={size} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
