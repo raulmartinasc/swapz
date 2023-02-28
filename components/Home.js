29bfd04bf3105f6012a008cc81827b8f3054448f
vincenzochiovaro <vincenzochiovaro194@gmail.com>
2023-02-28 08:25:16 +0000

    add refresh btn

--- components/Home.js
+++ components/Home.js
@@ -8,16 +8,17 @@ import {
   Dimensions,
   Platform,
 } from "react-native";
-import {doc, getDoc, collection, getDocs} from "firebase/firestore";
-import {db} from "../firebaseConfig";
-import {useEffect, useState, useContext} from "react";
-import {UserContext} from "../Context/UserContext";
+import { doc, getDoc, collection, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../Context/UserContext";
 
-const Home = ({navigation}) => {
-  const {userInfo, setUserInfo} = useContext(UserContext);
+const Home = ({ navigation }) => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
   const [items, setItems] = useState([]);
   const [displayUsername, setDisplayUsername] = useState("not loaded");
-  useEffect(() => {
+
+  const fetchItems = () => {
     getDocs(collection(db, "items")).then((result) => {
       setItems(
         result.docs.map((item) => {
@@ -25,34 +26,39 @@ const Home = ({navigation}) => {
         })
       );
     });
+  };
+
+  useEffect(() => {
+    fetchItems();
   }, []);
 
-  // useEffect(() => {
-  //   setDisplayUsername(userInfo.username);
-  // }, [items]);
   let key = 0;
 
   const navigateToItem = (item) => {
-    navigation.navigate("SingleItem", {item});
+    navigation.navigate("SingleItem", { item });
   };
 
   const numColumns = 3; // change this to change number of columns
   const screenWidth = Dimensions.get("window").width;
   const cardWidth = screenWidth / numColumns - 20; // subtracting 20 for margin
 
-  const renderCard = ({item}) => {
+  const renderCard = ({ item }) => {
     return (
       <TouchableOpacity
         onPress={() => {
           return navigateToItem(item);
         }}
       >
-        <View style={[styles.card, {width: cardWidth}]}>
-          <Image source={{uri: item.itemImg}} style={styles.cardImage} />
+        <View style={[styles.card, { width: cardWidth }]}>
+          <Image source={{ uri: item.itemImg }} style={styles.cardImage} />
           <Text style={styles.cardText}>{item.itemName} </Text>
         </View>
       </TouchableOpacity>
     );
+  };
+
+  const handleRefresh = () => {
+    fetchItems();
   };
 
   return (
@@ -63,6 +69,9 @@ const Home = ({navigation}) => {
         keyExtractor={(item, index) => index.toString()}
         numColumns={numColumns}
       />
+      <TouchableOpacity style={styles.refreshButton} onPress={handleRefresh}>
+        <Text style={styles.refreshButtonText}>Refresh</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -93,6 +102,18 @@ const styles = StyleSheet.create({
     fontSize: 10.1,
     padding: 5,
     textAlign: "center",
+    fontWeight: "bold",
+  },
+  refreshButton: {
+    backgroundColor: "#00ADEF",
+    padding: 10,
+    borderRadius: 50,
+    marginTop: 10,
+    marginBottom: 10,
+    alignSelf: "center",
+  },
+  refreshButtonText: {
+    color: "#fff",
     fontWeight: "bold",
   },
 });
